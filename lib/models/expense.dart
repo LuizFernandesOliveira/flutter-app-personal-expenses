3f83217f99921b26d8f04367afa6860da5c03690
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2022-09-22 21:44:07 -0300

    feat: create expenses

---
+++ lib/models/expense.dart
@@ -0,0 +1,13 @@
+class Expense {
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+
+  Expense({
+    required this.id,
+    required this.title,
+    required this.value,
+    required this.date,
+  });
+}
